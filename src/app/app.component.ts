64a3e7a8f162b7e742ad2ef6a61165a21926e52b
RafaelAlexandre007 <rafael.alexandre013@gmail.com>
2022-09-14 19:11:25 -0300

    Aula 10.1

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PRATICA-AULA10-DESAFIO2';
+  config: boolean = false;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ConfigComponent } from './config/config.component';
+import { TimerComponent } from './timer/timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ConfigComponent,
+    TimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
